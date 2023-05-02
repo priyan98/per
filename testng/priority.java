e0b1e20dfdbd0f37dbe1dd061315a90c4bcbb9f7
priyan98 <sangeethpriyanmnm@gamil.com>
2023-05-02 12:55:37 +0530

    reupload

--- testng/priority.java
+++ testng/priority.java
@@ -3,23 +3,35 @@ package com.testng;
 import org.testng.annotations.Test;
 
 public class priority {
-	
+
 	@Test(priority = 3)
-  private void sangeeth() {
+	private void sangeeth() {
 
-System.out.println("method 1");
-
-}
-	@Test(priority = 1)
-	  private void priyan() {
-
-	System.out.println("method 2");
+		System.out.println("method 1");
 
 	}
-	@Test(priority = 2)
-	  private void rithin() {
 
-	System.out.println("method 3");
+	@Test(priority = 1)
+	private void priyan() {
+
+		System.out.println("method 2");
+
+	}
+
+	@Test(priority = 2)
+	private void rithin() {
+
+		System.out.println("method 3");
+		
+		
+
+	}
+	@Test(priority = 4)
+	private void rithinS() {
+
+		System.out.println("method 3");
+		
+		
 
 	}
 }
