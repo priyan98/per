62b455277dc34e8c0c96bd724902b1e96c3c3b3f
sangeeth priyan <116938606+priyan98@users.noreply.github.com>
2023-05-02 12:52:47 +0530

    Update priority.java
    changed to the print

--- testng/priority.java
+++ testng/priority.java
@@ -7,19 +7,19 @@ public class priority {
 	@Test(priority = 3)
   private void sangeeth() {
 
-System.out.println("method 1");
+System.out.println("method one");
 
 }
 	@Test(priority = 1)
 	  private void priyan() {
 
-	System.out.println("method 2");
+	System.out.println("method Two");
 
 	}
 	@Test(priority = 2)
 	  private void rithin() {
 
-	System.out.println("method 3");
+	System.out.println("method Three");
 
 	}
 }
